87f27a72849149208a9eea8a16d0654be699198b
Ritish Adhikari <ritishadhikari@gmail.com>
2025-12-06 19:08:47 +0530

    Added Promethues Instrumentator

--- src/api/main.py
+++ src/api/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from schemas import HousePredictionRequest, PredictionResponse
 from inference import predict_price, batch_predict
 from typing import List
+from prometheus_fastapi_instrumentator import Instrumentator
 app=FastAPI(
     title="House Price Prediction API",
     description=(
@@ -27,6 +28,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
     )
+
+# Initialize and Instrument Prometheus metric
+Instrumentator().instrument(app=app).expose(app=app)
 
 @app.get("/health", response_model=dict)
 async def health_check():
